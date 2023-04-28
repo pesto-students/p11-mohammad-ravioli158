8f8b74107338bf833b28f0bfd8c165e3d9e6c122
ravioli158 <rathoreravi1508@gmail.com>
2023-04-28 19:02:28 +0530

    add playguessinggame method

--- Week-4/Session-09_JS_Basics/Assignments/JavaScript number guessing game/script.js
+++ Week-4/Session-09_JS_Basics/Assignments/JavaScript number guessing game/script.js
@@ -1,1 +1,20 @@
-// Your solution goes here
+function playGuessingGame(numToGuess, totalGuesses = 10) {
+  let guessCount = 0;
+  let guess = 0;
+  while (guessCount < totalGuesses && guess != numToGuess) {
+    // Prompt to get a guess from user
+    guess = prompt("Enter a number between 1 and 100");
+    guess = parseInt(guess);
+    // Validate user input
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+      alert("Please enter a valid number between 1 and 100.");
+      continue;
+    }
+    guessCount++;
+  }
+  if (guessCount <= totalGuesses && guess == numToGuess) {
+    return guessCount;
+  } else {
+    return 0;
+  }
+}
