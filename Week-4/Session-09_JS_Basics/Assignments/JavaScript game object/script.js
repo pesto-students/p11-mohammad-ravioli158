5a2ad071cf6f3e4843f2cf07cc7d906ec6de3a67
ravioli158 <rathoreravi1508@gmail.com>
2023-04-28 18:28:50 +0530

    add game object to task2

--- Week-4/Session-09_JS_Basics/Assignments/JavaScript game object/script.js
+++ Week-4/Session-09_JS_Basics/Assignments/JavaScript game object/script.js
@@ -1,4 +1,25 @@
-// Your solution goes here
+// Define a new object called game
+const game = {
+  // Initialize the properties of the game object
+  lives: 3, // the number of lives the player has, initially set to 3
+  coins: 0, // the number of coins the player has, initially set to 0
+
+  // Define a getter that calculates the number of points the player has earned
+  get points() {
+    return this.coins * 10; // each coin is worth 10 points
+  },
+
+  // Define a method that subtracts 1 from the lives property if the player has lives remaining
+  playerDies() {
+    this.lives > 0 ? this.lives-- : this.lives; // use a ternary operator to decrement lives if it is greater than 0
+  },
+
+  // Define a method that resets the lives and coins properties to their initial values
+  newGame() {
+    this.lives = 3; // reset lives to 3
+    this.coins = 0; // reset coins to 0
+  },
+};
 
 console.log("lives = " + game.lives); // should be 3
 console.log("coins = " + game.coins); // should be 0
