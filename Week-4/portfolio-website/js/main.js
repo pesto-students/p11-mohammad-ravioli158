ac742000d7e446bef8a0b79bd9a486e1fa435bb3
Ravi <rathoreravi1508@gmail.com>
2023-05-13 08:52:30 +0530

    Add comment to main.js and use variable names for css

--- Week-4/portfolio-website/js/main.js
+++ Week-4/portfolio-website/js/main.js
@@ -1,12 +1,12 @@
 // Toggle mobile menu to show and hide.
 let isMenuOpen = false;
-
+/* Toggles display mobile menu. Adds show-nav class to the element which increases width of the element */
 function toggleMobileMenu() {
-    const menu = document.getElementById('site-nav');
-    if (!menu) {
-        console.error("Could not find element with ID 'site-nav'");
-        return;
-    }
-    menu.classList.toggle('show-nav', !isMenuOpen);
-    isMenuOpen = !isMenuOpen;
+  const menu = document.getElementById("site-nav");
+  if (!menu) {
+    console.error("Could not find element with ID 'site-nav'");
+    return;
+  }
+  menu.classList.toggle("show-nav", !isMenuOpen);
+  isMenuOpen = !isMenuOpen;
 }
