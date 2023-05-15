82da32d911d6b956b1b9c1e45ec88fa9546c359c
Ravi <rathoreravi1508@gmail.com>
2023-05-15 09:03:28 +0530

    Complete CalcWordFrequencies

--- Week-4/Session-10_JavascriptBasics/JavaScript maps/script.js
+++ Week-4/Session-10_JavascriptBasics/JavaScript maps/script.js
@@ -1,3 +1,16 @@
+/* This function calculates frequency of each word in a Sentence */
 function calcWordFrequencies() {
-   
+  const input = prompt("Enter list of words separated by space");
+  //   Initialise empty map to store frequency of words
+  const word_freq = new Map();
+  // separate words into a array by splitting at space
+  const words = input.split(" ");
+  for (const word of words) {
+    // increase frequency of word if it is already in the map , or on first occurence set it to 1
+    word_freq.set(word, (word_freq.get(word) || 0) + 1);
+  }
+
+  for (const [word, frequency] of word_freq) {
+    console.log(`${word} ${frequency}`);
+  }
 }
