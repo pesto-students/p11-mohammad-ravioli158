51ec93b419cb5c64ea6b2af765d3be6dc6563bd3
Ravi <rathoreravi1508@gmail.com>
2023-05-22 19:49:02 +0530

    added memoization

--- Week-6/quote_generator/js/main.js
+++ Week-6/quote_generator/js/main.js
@@ -1,3 +1,4 @@
+const memoizedGenerateQuotes = memoize(generateQuotes);
 document.addEventListener("DOMContentLoaded", function () {
   const quoteForm = document.getElementById("topic-form");
   const topicInput = quoteForm["quote-topic"];
@@ -8,8 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (topic !== "") {
       clearQuotesList();
-      const quotes = await generateQuotes(topic);
+      const quotes = await memoizedGenerateQuotes(topic);
       addQuotes(quotes, topic);
     }
   });
 });
+
+//Memoization
+function memoize(f) {
+  const cache = new Map(); //value cache stored in closure
+  return function (...args) {
+    let key = args.length + args.join("+");
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    } else {
+      let result = f.apply(this, args);
+      cache.set(key, result);
+
+      return result;
+    }
+  };
+}
