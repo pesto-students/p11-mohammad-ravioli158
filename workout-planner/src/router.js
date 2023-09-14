a60d3b61609fde587a14a8e0093d677ff432bbd1
Ravi Rathore <rathoreravi1508@gmail.com>
2023-09-14 22:54:11 +0530

    fixed route

--- workout-planner/src/router.js
+++ workout-planner/src/router.js
@@ -2,16 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import WorkoutPlan from "./components/WorkoutPlan";
 import Layout from "./components/Layout";
 import CreatePlan from "./components/CreatePlan";
-import Profile from "./components/Profile";
 import NotFound from "./components/NotFound";
 import Workout from './components/Workout'
-const plan = null
+
 const router = createBrowserRouter([
     { path: "/", element: <Layout/>, errorElement: <NotFound/>, children:[
-    {path:'', element: (plan? <WorkoutPlan/> : <CreatePlan/>)}, 
+    {path:'', element: <WorkoutPlan/> }, 
     {path:'plan', element: <WorkoutPlan/>},
     {path:'plan/:day', element: <Workout/>},
     {path:'newplan', element: <CreatePlan/>},
-    {path:'profile', element: <Profile/>}
 ]} ]);
 export default router;
